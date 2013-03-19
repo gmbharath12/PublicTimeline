3ac6af8e9996a44d1476b23c953f27ffdd7f3b78
Bharath G M <bharath@Bharaths-MacBook-Pro.local>
2013-03-19 19:43:07 -0400

    Initial Commit

---
+++ PublicTimeline/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PublicTimeline
+//
+//  Created by Bharath G M on 3/19/13.
+//  Copyright (c) 2013 Bharath G M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PublicTimeline/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PublicTimeline
+//
+//  Created by Bharath G M on 3/19/13.
+//  Copyright (c) 2013 Bharath G M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
